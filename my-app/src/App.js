1d6e1484bd3f5bca531748a9363ce9cb9ea88c7c
Gurmali <Gurmali@hotmail.com>
2022-12-14 13:27:39 +0000

    rendering lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 import NavBarForm from './components/NavBarForm';
 import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
+import Content from './components/Content';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 {/*      <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
