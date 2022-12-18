da3fa802fdd3f02a72b0f206ed9d77b714b2a35b
Gurmali <Gurmali@hotmail.com>
2022-12-18 12:35:43 +0000

    searchbar

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,27 +10,50 @@ export class Content extends Component {
       super(props)
 
       this.state = {
-        isLoaded: false
+        isLoaded: false,
+        posts: []
       }
     
     }
     componentDidMount(){
       setTimeout(()=>{
         this.setState({
-          isLoaded: true
+          isLoaded: true,
+          posts: savedPosts
         })
       }, 2000)
     }
+
+    handleSearch = (event) => {
+      const name = event.target.value.toLowerCase()
+      console.log(name)
+      const filteredPosts = savedPosts.filter(post => {
+        return post.name.toLowerCase().includes(name)
+      })
+      this.setState({
+        posts: filteredPosts
+      })
+    }
+
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor="searchinput">Search:</label>
+              <input 
+              onChange={(event) => this.handleSearch(event)}
+              type="search" 
+              id="searchinput"
+              placeholder="By Author" />
+              <h4>posts found:{this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
           {
           this.state.isLoaded ?
-            <PostItem savedPosts={savedPosts} />
+            <PostItem savedPosts={this.state.posts} />
             : <Loader />
           }
         </div>
